5f2cb9567ceb2edb57ce4d74bd86fb45bddddeeb
hanseleslabon <35777083+hanseleslabon@users.noreply.github.com>
2019-12-07 13:04:52 -0500

    Initial Gig Project

---
+++ GigHub/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var upComingGigs = _context.Gigs
+                .Include(g=>g.Artist)
+                .Include(g=>g.Genre)
+                .Where(g=>g.DateTime > DateTime.Now);
+            return View(upComingGigs);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,38 @@
+﻿using GigHub.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int  Id { get; set; }
+
+        public ApplicationUser Artist { get; set; }
+
+        [Required]
+        public string ArtistId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; } 
+        public Genre Genre { get; set; }
+    }
+
+    public class Genre
+    {
+        public byte Id  { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+    }
+}

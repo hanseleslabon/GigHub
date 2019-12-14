149c1ef6c28dbe83760c4a0576c95da460253f3e
hanseleslabon <35777083+hanseleslabon@users.noreply.github.com>
2019-12-13 19:49:48 -0500

    Implement my gigs

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -17,6 +17,16 @@ namespace GigHub.Controllers
             _context = new ApplicationDbContext();
         }
 
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs.Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                 .Include(g=>g.Genre)
+                .ToList();
+
+            return View(gigs);
+        }
+
         [Authorize]
         public ActionResult Create()
         {
@@ -69,7 +79,7 @@ namespace GigHub.Controllers
             _context.Gigs.Add(gig);
             _context.SaveChanges();
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
         }
     }
 }

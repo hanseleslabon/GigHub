72b1cb05a85b19f4a83194978b1759583729270b
hanseleslabon <35777083+hanseleslabon@users.noreply.github.com>
2019-12-22 01:46:04 -0500

    Improved Notification

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -8,8 +8,12 @@ namespace GigHub
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/underscore-min.js",
+                        "~/Scripts/moment.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootbox.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,7 +29,8 @@ namespace GigHub
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/Site.css"));
+                      "~/Content/Site.css",
+                      "~/Content/animate.css"));
         }
     }
 }
